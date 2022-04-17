253f0071bebb17fa870d4e45e0dd2cc5235ce961
Adlar Tuten <adlartuten@Adlars-MacBook-Pro.local>
2022-04-17 16:18:25 -0400

    working sign up and login with SQL

--- public/login.js
+++ public/login.js
@@ -1,27 +1,28 @@
+// set up variables from HTML document
 let loginButton = document.getElementById("login");
 let username = document.getElementById("username");
 let password = document.getElementById("password");
+let message = document.getElementById("status");
 
+// login button function
 function login(event) {
-    event.preventDefault();
+    event.preventDefault(); // needed for custom actions
+    // set up XHR to send the data secretly
     let xhr = new XMLHttpRequest;
     xhr.addEventListener("load", responseHandler);
-    query = `username=${username.value}&password=${password.value}`;
-    url = `/attempt_login`;
-    xhr.responseType = "json";
-    xhr.open("POST", url);
+    query = `username=${username.value}&password=${password.value}`; // query string sent in JSON body
+    url = `/attempt_login`; // URL to go to when clicked (handled in express route in server.js)
+    xhr.responseType = "json"; // sets to use JSON data
+    xhr.open("POST", url); // must be POST because we are sending data
     xhr.setRequestHeader("Content-Type", "application/x-www-form-urlencoded");
-    xhr.send(query);
+    xhr.send(query); // send the request to the server
 }
 
+// function to run after XHR is done
 function responseHandler() {
-    let message = document.getElementById("message");
     message.style.display = "block";
-    if (this.response.success) {
-        message.innerText = "Success";
-    } else {
-        message.innerText = "Failure";
-    }
+    message.innerText = this.response.message; // sets the status text accordingly
 }
 
+// add the event listener to the login button
 loginButton.addEventListener("click", login);
--- server.js
+++ server.js
@@ -1,86 +1,104 @@
+// set up node requirements
 const express = require("express");
 const res = require("express/lib/response");
 const {json} = require("express/lib/response");
 const bcrypt = require("bcryptjs");
-const costFactor = 11;
-let authenticated = false;
 const mysql = require("mysql2");
 
+const costFactor = 11; // for bcrypt to encrypt passwords
+let authenticated = false; // status if user is logged in
+
+// connects to a local SQL database
+// here is the SQL code needed to create it:
+/*
+drop database if exists cs2803;
+create database cs2803;
+use cs2803;
+create table users(username varchar(64) primary key not null, password varchar(64) not null);
+*/
+
+// NOTE you may have to change password to match your mySQL password
 const conn = mysql.createConnection({
     host: "localhost",
     user: "root",
     password: "buzzbuzz123",
     database: "cs2803"
 })
-
-conn.connect(function(err) {
+conn.connect(function(err) { // establishes connection
     if (err) {
-        console.log("Error: ", err);
+        console.log("There was a mySQL error: ", err);
     } else {
-        console.log("Connected to database");
+        console.log("Connected to mySQL database");
     }
 });
 
+// create an instance of express web server
 const app = express();
-
-let user = "user";
-let pass = "pass";
-
+// directs express to use index.html file
 app.use(express.static("public"));
 
+// route for the home page
+app.get("/main", function(req, res) {
+    res.sendFile(__dirname + "/public/" + "index.html");
+});
+
+// route for the register page
 app.get("/register", function(req, res) {
     res.sendFile(__dirname + "/public/" + "register.html");
 });
 
+// route for the login page
+app.get("/login", function(req, res) {
+    res.sendFile(__dirname + "/public/" + "login.html");
+});
+
+// tells express to parse JSON data in POST requests
 app.use(express.urlencoded({extended: false}));
 
-app.post("/register", function(req, res) {
-    usernameQuery = "Select username from users where username = ?";
-    conn.query(usernameQuery, [req.body.username], function(err, rows) {
+// POST method for signing up
+app.post("/attempt_register", function(req, res) {
+    usernameQuery = "select username from users where username = ?"; // SQL statement
+    conn.query(usernameQuery, [req.body.username], function(err, rows) { // queries SQL statement with username as the ? value
         if (err) {
             res.json({success: false, message: "server error"})
         }
-
-        if (rows.length > 0) {
+        if (rows.length > 0) { // if the username was already found
             res.json({success: false, message: "username taken"})
-        } else {
-            passwordHash = bcrypt.hashSync(req.body.password, costFactor);
-            insertUser = "insert into Users values(?, ?)"
-            conn.query(insertUser, [req.body.username, passwordHash], function(err, rows) {
+        } else { // otherwise create the user
+            passwordHash = bcrypt.hashSync(req.body.password, costFactor); // encrypts the password
+            insertUser = "insert into users values(?, ?)" // SQL statement
+            conn.query(insertUser, [req.body.username, passwordHash], function(err, rows) { // stores username and password
                 if (err) {
                     res.json({success: false, message: "server error"})
                 } else {
                     res.json({success: true, message: "user registered"})
                 }
-            })
+            });
         }
     });
-})
+});
 
-
-
+// POST method for logging in
 app.post("/attempt_login", function(req, res){
-    // we check for the username and password to match.
-    conn.query("select password from users where username = ?", [req.body.username], function (err, rows){
+    conn.query("select password from users where username = ?", [req.body.username], function (err, rows){ // SQL query
         if (err) {
-            res.json({success: false, message: "user doesn't exists"});
+            res.json({success: false, message: "Sorry, we couldn't find a user with that username."});
+        } 
+        if (rows.length < 1) {
+            res.json({success: false, message: "Sorry, we couldn't find a user with that username."});
         } else {
-            storedPassword = rows[0].password // rows is an array of objects e.g.: [ { password: '12345' } ]
-            // bcrypt.compareSync let's us compare the plaintext password to the hashed password we stored in our database
-            if (bcrypt.compareSync(req.body.password, storedPassword)){
-                authenticated = true;
-                res.json({success: true, message: "logged in"})
+            storedPassword = rows[0].password // gets the hashed password that was stored in the SQL database
+            if (bcrypt.compareSync(req.body.password, storedPassword)){ // checks the plaintext with the hash using scary math
+                authenticated = true; // set the user to be logged in (currently doesn't actually do anything)
+                res.json({success: true, message: "Successfully logged in!"});
             } else {
-                res.json({success: false, message:"password is incorrect"})
+                res.json({success: false, message:"Wrong password bozo!"});
             }
         }
-    })  
-})
+    });
+});
 
-app.get("/main", function(req, res) {
-    res.sendFile(__dirname + "/public/" + "index.html");
-})
-
+// starts the express web server
 app.listen(3000, function() {
     console.log("Listening on port 3000...");
 });
